b4e30e46a6450756a32a258bde05a796b782c589
Abhishek Kumar Gupta <its.diamond8990@gmail.com>
2026-07-24 14:49:55 +0530

    Time: 5 ms (80.95%), Space: 45.8 MB (87.57%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n = x;
+        int rev = 0;
+        while(n>0){
+            int r = n%10;
+            rev = rev*10+r;
+            n = n/10;
+        }
+        if(x==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
