8e108a29f25f06328e71fc66fe04c2fb88e9935f
Abhishek Kumar Gupta <its.diamond8990@gmail.com>
2026-08-24 22:41:42 +0530

    Time: 1 ms (99.48%), Space: 44.5 MB (63.31%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
@@ -2,13 +2,7 @@ class Solution {
     public int findNumbers(int[] nums) {
         int c = 0;
         for(int i : nums){
-            // int d = 0;
-            // while(i > 0){
-            //     i = i/10;
-            //     d++;
-            // }
-            String a = i+"";
-            int d = a.length();
+            int d = (int)(Math.log10(i))+1;
             if(d % 2 == 0)
                 c++;
         }
